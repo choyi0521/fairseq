311c56520263c1aaa2e8debaa639e8cf6c13ad32
choyi0521 <choyi0521@gmail.com>
2019-11-15 23:52:24 +0900

    disable unfold

--- fairseq/modules/ddynamic_convolution.py
+++ fairseq/modules/ddynamic_convolution.py
@@ -100,7 +100,7 @@ class DDynamicConv1dTBC(nn.Module):
 
         if query is None:
             query = x
-        if unfold:
+        if True:  # unfold:
             output = self._forward_unfolded(x, incremental_state, query)
         else:
             output = self._forward_expanded(x, incremental_state, query)
