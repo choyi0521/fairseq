cdd4fc543e584caa10ee13289c5d1606e8218302
choyi0521 <choyi0521@gmail.com>
2019-11-16 21:16:38 +0900

    Update ddynamic_convolution.py

--- fairseq/modules/ddynamic_convolution.py
+++ fairseq/modules/ddynamic_convolution.py
@@ -119,7 +119,7 @@ class DDynamicConv1dTBC(nn.Module):
                 outputs.append(self._forward_unfolded(tx.view(T,B,C), incremental_state, query.narrow(2, i*Q, Q), self.weight_linears[i]))
             else:
                 outputs.append(self._forward_expanded(tx.view(T,B,C), incremental_state, query.narrow(2, i*Q, Q), self.weight_linears[i]))
-            tx = torch.stack([tx[:, :, :, j].roll(j*roll, 2) for j in range(R)], 3)
+            #tx = torch.stack([tx[:, :, :, j].roll(j*roll, 2) for j in range(R)], 3)
 
         output = self.output_linear(torch.stack(outputs, 3)).squeeze(3)
 
